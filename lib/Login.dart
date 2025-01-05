971771bf613c3af8dd48ba0e1ccc36d0d499d505
ARUNKUMAR-0984 <arunkumars97462@gmail.com>
2025-01-05 13:13:32 +0530

    Login Page Modified

--- lib/Login.dart
+++ lib/Login.dart
@@ -6,7 +6,7 @@ class Login extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Login Page"),
+        title: const Text("Login Page Data Changed"),
       ),
     );
   }
